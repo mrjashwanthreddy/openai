49eaa9b15e4301b0ccfed2b879f87eb1929f5521
mr.jashwanthreddy <mr.jashwanthreddy@gmail.com>
2025-12-16 08:24:38 +0530

    Added OPENAI, OLLAMA and DOCKER MODEL integrations

---
+++ src/main/java/com/openai/config/ChatClientConfig.java
@@ -0,0 +1,23 @@
+package com.openai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
+        ChatClient.Builder chatClientBuilder = ChatClient.builder(ollamaChatModel);
+        return chatClientBuilder.build();
+    }
+
+    @Bean
+    public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel) {
+        return ChatClient.create(openAiChatModel);
+    }
+
+}
