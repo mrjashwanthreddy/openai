74b34b17472e7f3e181e563050b52f9a0b2922b1
mr.jashwanthreddy <mr.jashwanthreddy@gmail.com>
2025-12-19 11:11:32 +0530

    Added Chat Memory using H2 Database

--- src/main/java/com/openai/config/ChatClientConfig.java
+++ src/main/java/com/openai/config/ChatClientConfig.java
@@ -2,7 +2,12 @@ package com.openai.config;
 
 import com.openai.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.ollama.OllamaChatModel;
 //import org.springframework.ai.openai.OpenAiChatModel;
@@ -13,6 +18,23 @@ import java.util.List;
 
 @Configuration
 public class ChatClientConfig {
+
+    // creating
+    @Bean
+    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder().maxMessages(10)
+                .chatMemoryRepository(jdbcChatMemoryRepository).build();
+    }
+
+    // creating chat client with chat memory config
+    @Bean
+    public ChatClient chatMemoryChatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        Advisor messageChatMemoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        return chatClientBuilder
+                .defaultAdvisors(List.of(loggerAdvisor, messageChatMemoryAdvisor))
+                .build();
+    }
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
